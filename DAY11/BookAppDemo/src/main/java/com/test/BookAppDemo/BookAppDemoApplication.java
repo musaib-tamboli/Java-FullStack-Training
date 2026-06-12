17cf393944fa130cd345ccca21ec6d8dc47834d1
royalmusaib <musaibtamboli28@gmail.com>
2026-06-13 00:16:15 +0530

    Initial Spring Boot project setup

---
+++ DAY11/BookAppDemo/src/main/java/com/test/BookAppDemo/BookAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.test.BookAppDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookAppDemoApplication.class, args);
+	}
+
+}
---
+++ DAY11/BookAppDemo/src/test/java/com/test/BookAppDemo/BookAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.BookAppDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
